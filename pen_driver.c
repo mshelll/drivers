55eb4e4608045de1d682579b17b46d061e696042
Marshel <Marshel.Abraham@in.bosch.com>
2014-05-03 14:31:02 +0530

    Update pen_driver.c

--- pen_driver.c
+++ pen_driver.c
@@ -130,5 +130,5 @@ module_init(pen_init);
 module_exit(pen_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Anil Kumar Pugalia <email_at_sarika-pugs_dot_com>");
+MODULE_AUTHOR("");
 MODULE_DESCRIPTION("USB Pen Registration Driver");
